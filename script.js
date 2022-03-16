8a496bc1650bc51ce789d3c41fdf5718972e4824
jeffrey burke <burkejeffrey56@yahoo.com>
2022-03-16 19:15:40 -0400

    Second commit. trying onclick event

---
+++ script.js
@@ -0,0 +1,19 @@
+/* 
+function -Create a Github repository for this project. 
+-The count starts at 0
+-Have two buttons on the screen, one to increase (+) the count by 1, and one button to lower (-) the count by 1.
+-Pressing the plus button increases the count by 1, pressing the minus button decreases by 1
+-Create a Github repository for this project and write Git commits as you go.
+-Send me the project for review and get started on the next project.
+have a starting variable that is equal to 0
+increase the count when pressing + by adding 1 to the current number at
+the same time decrease the count by 1 when - is pressed 
+
+
+*/
+ function addNumber(){
+ 	document.getElementById("demo").innerHTML = 
+
+	let currentNumber= 0
+	return newNumber = currentNumber + 1
+}
